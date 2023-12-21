8f366dc8bb56c9e2e3ad4c026256dc7cc5bf6cbb
strdos <81235866+strdos@users.noreply.github.com>
2023-12-21 15:01:52 +0100

    composition example (SmartKitchen)

---
+++ src/Composition/CoffeeMaker.java
@@ -0,0 +1,28 @@
+package Composition;
+
+public class CoffeeMaker {
+
+    private boolean hasWorkToDo;
+
+    public CoffeeMaker(boolean hasWorkToDo) {
+        this.hasWorkToDo = hasWorkToDo;
+    }
+
+    public CoffeeMaker() {
+    }
+
+    public boolean isHasWorkToDo() {
+        return hasWorkToDo;
+    }
+
+    public void setHasWorkToDo(boolean hasWorkToDo) {
+        this.hasWorkToDo = hasWorkToDo;
+    }
+
+    public void brewCoffee() {
+        if (hasWorkToDo) {
+            System.out.println("brewing coffee...");
+            hasWorkToDo = false;
+        }
+    }
+}
