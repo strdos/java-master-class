50ab54f2a6e2452ff7888edccdd06cf399537a63
strdos <81235866+strdos@users.noreply.github.com>
2024-01-14 22:29:30 +0100

    Playlist challenge (nested classes practice)

---
+++ src/nested_classes/Album.java
@@ -0,0 +1,120 @@
+package nested_classes;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Album {
+
+    public static void main(String[] args) {
+
+        List<Album> albums = new ArrayList<>();
+
+        Album album = new Album("Stormbringer", "Deep Purple");
+        album.addSong("Stormbringer", 4.6);
+        album.addSong("Love don't mean a thing", 4.22);
+        album.addSong("Holy man", 4.3);
+        album.addSong("Hold on", 5.6);
+        album.addSong("Lady double dealer", 3.21);
+        album.addSong("You can't do it right", 6.23);
+        album.addSong("High ball shooter", 4.27);
+        album.addSong("The gypsy", 4.2);
+        album.addSong("Soldier of fortune", 3.13);
+        albums.add(album);
+
+        album = new Album("For those about to rock", "AC/DC");
+        album.addSong("For those about to rock", 5.44);
+        album.addSong("I put the finger on you", 3.25);
+        album.addSong("Lets go", 3.45);
+        album.addSong("Inject the venom", 3.33);
+        album.addSong("Snowballed", 4.51);
+        album.addSong("Evil walks", 3.45);
+        album.addSong("C.O.D.", 5.25);
+        album.addSong("Breaking the rules", 5.32);
+        album.addSong("Night of the long knives", 5.12);
+        albums.add(album);
+
+        LinkedList<Song> playList = new LinkedList<Song>();
+        albums.get(0).addToPlayList("You can't do it right", playList);
+        albums.get(0).addToPlayList("Holy man", playList);
+        albums.get(0).addToPlayList("Speed king", playList);  // Does not exist
+        albums.get(0).addToPlayList(9, playList);
+        albums.get(1).addToPlayList(8, playList);
+        albums.get(1).addToPlayList(3, playList);
+        albums.get(1).addToPlayList(2, playList);
+        albums.get(1).addToPlayList(24, playList);  // There is no track 24
+    }
+
+    private String name;
+    private String artist;
+    private SongList songs;
+
+    public Album(String name, String artist) {
+        this.name = name;
+        this.artist = artist;
+        this.songs = new SongList();
+    }
+
+    public boolean addSong(String title, double duration) {
+        return songs.add(new Song(title, duration));
+    }
+
+    public boolean addToPlayList(int trackNumber, LinkedList<Song> playList) {
+
+        Song checkedSong = songs.findSong(trackNumber);
+        if (checkedSong != null) {
+            playList.add(checkedSong);
+            return true;
+        }
+        System.out.println("This album does not have a track " + trackNumber);
+        return false;
+    }
+
+    public boolean addToPlayList(String title, LinkedList<Song> playList) {
+
+        Song checkedSong = songs.findSong(title);
+        if (checkedSong != null) {
+            playList.add(checkedSong);
+            return true;
+        }
+        System.out.println("The song " + title + " is not in this album");
+        return false;
+    }
+
+    public static class SongList {
+
+        private ArrayList<Song> songs;
+
+        private SongList() {
+            songs = new ArrayList<Song>();
+        }
+
+        private boolean add(Song song) {
+
+            if (songs.contains(song)) {
+                return false;
+            }
+            songs.add(song);
+            return true;
+        }
+
+        private Song findSong(String title) {
+
+            for (Song checkedSong : songs) {
+                if (checkedSong.getTitle().equals(title)) {
+                    return checkedSong;
+                }
+            }
+            return null;
+        }
+
+        private Song findSong(int trackNumber) {
+
+            int index = trackNumber - 1;
+            if ((index > 0) && (index < songs.size())) {
+                return songs.get(index);
+            }
+            return null;
+        }
+    }
+}
